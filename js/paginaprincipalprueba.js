51f1ff1ae70a6470a1ae8a48a817826e248cc642
luischehin <luischehinok@gmail.com>
2023-06-12 22:34:49 -0300

    poninendo cada tarjeta

--- js/paginaprincipalprueba.js
+++ js/paginaprincipalprueba.js
@@ -1,3 +1,4 @@
+/// LO MAS BUSCADO //
 const lomasbuscado= [
     {name: 'Pizza Napolitana',
      price: 1800,
@@ -28,4 +29,43 @@ lomasbuscado.forEach(function (buscado) {
     
     card.innerHTML = item;
     buscadoSection.appendChild(card);
+})
+ /// PIZZAS ////
+
+const pizzas= [
+    {name:'Pizza Napolitana',
+     price: 1800,
+     url: 'https://avantipizzas.com.ar/wp-content/uploads/2022/04/napolitana-con-albahaca-pizzas-avanti-300x300.jpg'},
+    {name:'Pizza con peperoni',
+     price: 2000,
+     url: 'https://www.labrigatta.com/wp-content/uploads/2021/02/product_19-300x300.jpg'},
+    {name:'Pizza con champiniones',
+     price: 2200,
+     url: 'https://maxipasta.com.ar/wp-content/uploads/2022/05/Pizza_Champi-300x300.jpg'},
+    {name:'PIzza de Calabresa',
+     price: 2100,
+     url: 'https://avantipizzas.com.ar/wp-content/uploads/2022/04/calabressa-pizzas-avanti-300x300.jpg'},
+     {name:'Pizza de Fugazzeta',
+      price: 2100,
+      url:'https://maxipasta.com.ar/wp-content/uploads/2022/05/fugazzeta-rellena-1-300x300.jpg'},
+      {name:'Pizza de ternera',
+       price:'2300',
+       url:'https://st4.depositphotos.com/1000605/21101/i/450/depositphotos_211012608-stock-photo-pizza-with-minced-meat-and.jpg'},
+]
+
+const pizzaSection = document.querySelector('#pizzas');
+
+pizzas.forEach(function (Pizza) {
+    let card = document.createElement('div');
+    card.classList.add('card')
+
+    let item = ` 
+    <img src=${Pizza.url} alt=${Pizza.name}/>
+    <p>${Pizza.name}</p>
+    <p>${Pizza.price}</p>
+    `;
+
+    card.innerHTML = item;
+    pizzaSection.appendChild(card);
+    
 })
