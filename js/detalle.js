5facf590b29939656283514dfb1bbbd0330232d3
Nassahel Elias <79935315+nassahel@users.noreply.github.com>
2023-06-14 15:13:28 -0300

    creados nuevamente las tarjetas con javascript

--- js/detalle.js
+++ js/detalle.js
@@ -1,38 +1,38 @@
-let carrito = [];
+let carrito = []
 
+let productos = JSON.parse(localStorage.getItem("carritox"));
 
-let agregarProd = document.getElementById("agregarProducto")
-agregarProd.addEventListener("click", addProducto)
-
-
-
-function addProducto() {
-    
-    let nombreProducto = document.getElementById("nombreProducto").innerText; 
-    let precioProducto = document.getElementById("precioProducto").innerText; 
-    let codigoProducto = document.getElementById("codigoProducto").innerText;
-    let cantidadProducto = document.getElementById("cantidadProducto").value;
-    let modalBody = document.getElementById("modalBody");
-
-
-carrito.push({
-    nombre: nombreProducto,
-    precio: precioProducto,
-    codigo: codigoProducto
-})
-
-
-let nuevoProducto = document.createElement("div")
-nuevoProducto.className =  "itemCarrito"
-nuevoProducto.innerHTML = parseInt(cantidadProducto) * `
-<p>${nombreProducto}</p> 
-<p>${precioProducto}</p> 
-<p>${codigoProducto}</p> 
-`
-
-console.log(carrito);
-
-modalBody.append(carrito)
+if(productos === null) {
+    productos = sandwichs;
+    localStorage.setItem("carritox", JSON.stringify(productos))
 }
 
+function crearTarjeta(arrayProductos, seccion) {
+    arrayProductos.forEach(producto => {
+        
+        let item = `
+        <section class="row col-11 p-2 new-sand bg-white my-4 shadow row d-flex justify-content-between">
+        <img class="col col-12 col-lg-3" src=${producto.Imagen}>
+        <div class="col col-12 col-lg-6">
+            <h2>${producto.Nombre}</h2> 
+            <h2>$${producto.Precio}</h2>  
+            <p>${producto.Descripcion}</p>  
+            <p>#${producto.ID}</p>
+        </div>
+        <div class="col col-12 col-lg-3 m-auto text-center">
+        <button class="btn btn-agregar">Agregar al Carrito</button>
+        </div>
+        </section>
+        
+        `;
+        seccion.innerHTML += item; 
+    });       
+}
 
+function renderTarjeta() {
+    let pizzaBox = document.getElementById("pizza-box")
+
+    crearTarjeta(sandwichs, pizzaBox)
+}
+
+renderTarjeta()
