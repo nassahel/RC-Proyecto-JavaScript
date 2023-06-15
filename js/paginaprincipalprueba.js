5b7668dfa676ec9f78201298ccd4d2e22a71e754
luischehin <luischehinok@gmail.com>
2023-06-15 13:03:17 -0300

    ya puse los botones de los precios, me falta hacer bien lo bordes de las card

--- js/paginaprincipalprueba.js
+++ js/paginaprincipalprueba.js
@@ -3,16 +3,16 @@
 /// LO MAS BUSCADO //
 const lomasbuscado= [
     {name: 'Pizza Napolitana',
-     price: 1800,
+     price: '$1800',
      url:'https://avantipizzas.com.ar/wp-content/uploads/2022/04/napolitana-con-albahaca-pizzas-avanti-300x300.jpg'},
     {name:'Empanadas de ternera',
-     price: 2000,
+     price: '$2000',
      url:'https://tiendadeempanadas.com.ar/wp-content/uploads/2020/07/POLlO-300x300.jpg'},
     {name:'Coca cola 2lts',
-     price: 850,
+     price: '$850',
      url:'https://trajineras.com/wp-content/uploads/2020/02/cocacola_2.5lts.png'},
      {name:'Cerveza Quilmes',
-     price: 700,
+     price: '$700',
      url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrVXyPS5zCxuNgSQneFSpZek0whs9ZGHFV1g&usqp=CAU',
      },
 ]
@@ -27,7 +27,7 @@ lomasbuscado.forEach(function (buscado) {
     let item =`
     <img src=${buscado.url} alt=${buscado.name}/>
     <p>${buscado.name}</p>
-    <p>${buscado.price}</p>
+    <button type="submit">${buscado.price}</button>
     `;
     
     card.innerHTML = item;
@@ -37,16 +37,16 @@ lomasbuscado.forEach(function (buscado) {
 
 const pizzas= [
     {name:'Pizza Napolitana',
-     price: 2350,
+     price: '$2350',
      url: 'https://avantipizzas.com.ar/wp-content/uploads/2022/04/napolitana-con-albahaca-pizzas-avanti-300x300.jpg'},
     {name:'Pizza de muzzarella',
-     price: 2300,
+     price: '$2300',
      url: 'https://avantipizzas.com.ar/wp-content/uploads/2022/04/roquefort-pizzas-avanti-300x300.jpg'},
     {name:'PIzza de Calabresa',
-     price: 2500,
+     price: '$2500',
      url: 'https://avantipizzas.com.ar/wp-content/uploads/2022/04/calabressa-pizzas-avanti-300x300.jpg'},
     {name:'Pizza de Fugazzeta',
-     price: 2300,
+     price: '$2300',
      url:'https://maxipasta.com.ar/wp-content/uploads/2022/05/fugazzeta-rellena-1-300x300.jpg'},
 ]
 
@@ -59,7 +59,7 @@ pizzas.forEach(function (Pizza) {
     let item = ` 
     <img src=${Pizza.url} alt=${Pizza.name}/>
     <p>${Pizza.name}</p>
-    <p>${Pizza.price}</p>
+    <button type="submit">${Pizza.price}</button>
     `;
 
     card.innerHTML = item;
@@ -70,19 +70,19 @@ pizzas.forEach(function (Pizza) {
 
 const minutas=[ 
     {name:'Pechuga c/guarnicion',
-    price: 2100,
+    price: '$2100',
     url: 'https://chefmiralles.files.wordpress.com/2014/02/pollomanzana-7.jpg?w=300&h=300',
     },
     {name:'Bife de lomo',
-    price: 1600,
+    price: '$1600',
     url:'https://dlvry-tkout.com/wp-content/uploads/2022/04/3295-300x300.jpg',
     },
     {name:'Kipe relleno',
-    price: 2300,
+    price: '$2300',
     url:'https://www.cucinare.tv/wp-content/uploads/2019/09/Keppe-300x300.jpg',
     },
     {name:'Matambre al verdeo',
-    price: 2500,
+    price: '$2500',
     url:'https://img-global.cpcdn.com/recipes/d974bacedda743ae/400x400cq70/photo.jpg',
     },
 ]
@@ -95,7 +95,7 @@ minutas.forEach(function (Minutas) {
     let item = ` 
     <img src=${Minutas.url} alt=${Minutas.name}/>
     <p>${Minutas.name}</p>
-    <p>${Minutas.price}</p>
+    <button type="submit">${Minutas.price}</button>
     `;
 
     card.innerHTML = item;
@@ -106,19 +106,19 @@ minutas.forEach(function (Minutas) {
 
 const empanadas=[
     {name:'Empanada de carne',
-     price: 240,
+     price: '$240',
      url:'https://sp-ao.shortpixel.ai/client/to_webp,q_lossless,ret_img,w_250,h_250/https://www.recetasjudias.com/wp-content/uploads/2017/06/Empanadas-Argentinas-300x300.jpg',
     },
     {name: 'Empanada de pollo',
-     price: 240,
+     price: '$240',
      url:'https://www.domenico.es/wp-content/uploads/2022/12/DSC07161-scaled-e1671539130174-300x300.jpg',
     },
     {name:'Empanada de capresse',
-     price: 250,
+     price: '$250',
      url:'https://www.deliciosi.com/images/100/134/empanadas-caprese.jpg',
     },
     {name:'Sfijas',
-    price: 240,
+    price: '$240',
     url:'https://arabianshawarma.com.ar/wp-content/uploads/2021/11/sfijas--300x300.jpg',
     }
 
@@ -133,7 +133,7 @@ empanadas.forEach(function (Empanadas) {
     let item = ` 
     <img src=${Empanadas.url} alt=${Empanadas.name}/>
     <p>${Empanadas.name}</p>
-    <p>${Empanadas.price}</p>
+    <button type="submit">${Empanadas.price}</button>
     `;
 
     card.innerHTML = item;
@@ -144,19 +144,19 @@ empanadas.forEach(function (Empanadas) {
 
 const sandwiches= [
     {name:'Sandwich de Milanesa',
-     price: 1500,
+     price: '$1500',
      url:'https://www.shutterstock.com/image-photo/sandwich-de-milanesa-con-huevo-260nw-2178793257.jpg'
     },
     {name:'Sandwich de Ternera',
-    price: 1500,
+    price: '$1500',
     url:'https://img-global.cpcdn.com/recipes/9efa1e15484d1a5d/400x400cq70/photo.jpg',
     },
     {name:'Sandwich de Lomito',
-    price: 1600,
+    price: '$1600',
     url:'https://cdn.shopify.com/s/files/1/0596/4313/4144/products/image_a2c57cce-32d6-46b0-aeef-933d3272cb07.jpg?v=1649356971',
     },
     {name:'Hamburguesa comun',
-    price: 1400,
+    price: '$1400',
     url:'https://www.baarty.com/articulos/wp-content/uploads/2022/07/proveedor-pan-hamburguesas.jpg',
     },
 ]
@@ -169,7 +169,7 @@ sandwiches.forEach(function (Sandiwiches) {
     let item = ` 
     <img src=${Sandiwiches.url} alt=${Sandiwiches.name}/>
     <p>${Sandiwiches.name}</p>
-    <p>${Sandiwiches.price}</p>
+    <button type="submit">${Sandiwiches.price}</button>
     `;
 
     card.innerHTML = item;
@@ -179,19 +179,19 @@ sandwiches.forEach(function (Sandiwiches) {
 /// BEBIDAS //
 const bebidas=[
     {name:'Coca cola 2lts',
-    price: 600,
+    price: '$600',
     url:'https://trajineras.com/wp-content/uploads/2020/02/cocacola_2.5lts.png',
     },
     {name:'Sprite 2lts',
-     price: 500,
+     price: '$500',
      url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9-ifDmZIa7bGnTadszbb2R6-NnXcJ4XAyAgT0iDBHwByNxjTtUr8kxRqqEEDKHmZvG2w&usqp=CAU',
     },
     {name:'Agua Mineral',
-     price: 400,
+     price: '$400',
      url:'https://blogautoservicios.com.ar/wp-content/uploads/2023/04/7790315000422_536-7.jpg',
     },
     {name:'Cerveza Quilmes',
-     price: 700,
+     price: '$700',
      url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrVXyPS5zCxuNgSQneFSpZek0whs9ZGHFV1g&usqp=CAU',
     }
 ]
@@ -205,7 +205,7 @@ bebidas.forEach(function (Bebidas) {
     let item = ` 
     <img src=${Bebidas.url} alt=${Bebidas.name}/>
     <p>${Bebidas.name}</p>
-    <p>${Bebidas.price}</p>
+    <button type="submit">${Bebidas.price}</button>
     `;
 
     card.innerHTML = item;
