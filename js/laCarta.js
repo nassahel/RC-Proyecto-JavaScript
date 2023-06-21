e7638ea6c7f94b61e7adfcced286cec59a670c0b
Nassahel Elias <79935315+nassahel@users.noreply.github.com>
2023-06-20 21:20:13 -0300

    se hizo merge con rama de descripcion de productos

--- js/laCarta.js
+++ js/laCarta.js
@@ -3,42 +3,36 @@ let empanadas = [
     Descripcion: "Empanadas de carne cortada a cuchillo, con huevo. Cocinadas en horno de barro",
     Precio: "240",
     ID: 3241,
-    Categoria: "Empanadas",
     Imagen: "https://www.laalemanacomidas.com.ar/wp-content/uploads/2020/04/180786-a3dd8eb3-b0db-4b1d-bf9f-83c55bc38593-1531908931.png"
 },
 {Nombre: "Empanadas de Pollo",
     Descripcion: "Empanadas de pollo cortado a cuchillo, con huevo. Cocinadas en horno de barro",
     Precio: "240",
     ID: 3242,
-    Categoria: "Empanadas",
     Imagen: "https://www.laalemanacomidas.com.ar/wp-content/uploads/2020/04/180786-a3dd8eb3-b0db-4b1d-bf9f-83c55bc38593-1531908931.png"
 },
 {Nombre: "Empanadas de Mondongo",
     Descripcion: "Empanadas de Mondongo cortado a cuchillo. Cocinadas en horno de barro",
     Precio: "240",
     ID: 3243,
-    Categoria: "Empanadas",
     Imagen: "https://www.laalemanacomidas.com.ar/wp-content/uploads/2020/04/180786-a3dd8eb3-b0db-4b1d-bf9f-83c55bc38593-1531908931.png"
 },
 {Nombre: "Empanadas Capresse",
     Descripcion: "Empanadas de omate, albahaca y queso, con aceite e oliva y condimentadas con hiervas. Cocinadas en horno de barro",
     Precio: "230",
     ID: 3244,
-    Categoria: "Empanadas",
     Imagen: "https://www.laalemanacomidas.com.ar/wp-content/uploads/2020/04/180786-a3dd8eb3-b0db-4b1d-bf9f-83c55bc38593-1531908931.png"
 },
 {Nombre: "Empanadas de Jamon y Queso",
     Descripcion: "Empanadas de Jamon primera calidad y Mozzarela, condimentadas con aceite de oliva y oregano. Cocinadas en horno de barro",
     Precio: "250",
     ID: 3245,
-    Categoria: "Empanadas",
     Imagen: "https://www.laalemanacomidas.com.ar/wp-content/uploads/2020/04/180786-a3dd8eb3-b0db-4b1d-bf9f-83c55bc38593-1531908931.png"
 },
 {Nombre: "Sfijas",
     Descripcion: "Empanadas Arabes, de carne molida, con cebolla y tomate, condimentadas con especias arabes. Cocinadas en horno de barro",
     Precio: "240",
     ID: 3246,
-    Categoria: "Empanadas",
     Imagen: "https://www.laalemanacomidas.com.ar/wp-content/uploads/2020/04/180786-a3dd8eb3-b0db-4b1d-bf9f-83c55bc38593-1531908931.png"
 }]
 
@@ -48,42 +42,36 @@ let pizzas = [
     Descripcion: "Pizza con muzzarella, aceitunas y orégano. Cocinada en horno de barro",
     Precio: "2300",
     ID: 4241,
-    Categoria: "Pizzas",
     Imagen: "https://www.laalemanacomidas.com.ar/wp-content/uploads/2020/04/180786-a3dd8eb3-b0db-4b1d-bf9f-83c55bc38593-1531908931.png"
 },
 {Nombre: "Pizza Fugazzeta",
     Descripcion: "Pizza con muzzarella, cebolla, aceitunas y orégano. Cocinada en horno de barro",
     Precio: "2400",
     ID: 4242,
-    Categoria: "Pizzas",
     Imagen: "https://www.laalemanacomidas.com.ar/wp-content/uploads/2020/04/180786-a3dd8eb3-b0db-4b1d-bf9f-83c55bc38593-1531908931.png"
 },
 {Nombre: "Pizza Especial",
     Descripcion: "Pizza con muzzarella, jamon, morrones, aceitunas y orégano. Cocinada en horno de barro",
     Precio: "2500",
     ID: 4243,
-    Categoria: "Pizzas",
     Imagen: "https://www.laalemanacomidas.com.ar/wp-content/uploads/2020/04/180786-a3dd8eb3-b0db-4b1d-bf9f-83c55bc38593-1531908931.png"
 },
 {Nombre: "Pizza Calabresa",
     Descripcion: "Pizza con muzzarella, cantimpalo, aceitunas y orégano. Cocinada en horno de barro",
     Precio: 2700,
     ID: 4244,
-    Categoria: "Pizzas",
     Imagen: "https://www.laalemanacomidas.com.ar/wp-content/uploads/2020/04/180786-a3dd8eb3-b0db-4b1d-bf9f-83c55bc38593-1531908931.png"
 },
 {Nombre: "Pizza Napolitana",
     Descripcion: "Pizza con muzzarella, rodajas de tomate, chimi de perejil y ajo, aceitunas y orégano. Cocinada en horno de barro",
     Precio: 2500,
     ID: 4245,
-    Categoria: "Pizzas",
     Imagen: "https://www.laalemanacomidas.com.ar/wp-content/uploads/2020/04/180786-a3dd8eb3-b0db-4b1d-bf9f-83c55bc38593-1531908931.png"
 },
 {Nombre: "Pizza de Ternera",
     Descripcion: "Pizza con muzzarella, fetas de ternera con sala inglesa, aceitunas y orégano. Cocinada en horno de barro",
     Precio: 2600,
     ID: 4246,
-    Categoria: "Pizzas",
     Imagen: "https://www.laalemanacomidas.com.ar/wp-content/uploads/2020/04/180786-a3dd8eb3-b0db-4b1d-bf9f-83c55bc38593-1531908931.png"
 }]
 
@@ -92,42 +80,36 @@ let minutas = [
     Descripcion: "Milanesa rellena con muzzarela y jamon, con guarnición a elección: Papas Fritas, Pure o ensalada",
     Precio: 2300,
     ID: 5241,
-    Categoria: "Minutas",
     Imagen: "https://www.laalemanacomidas.com.ar/wp-content/uploads/2020/04/180786-a3dd8eb3-b0db-4b1d-bf9f-83c55bc38593-1531908931.png"
 },
 {Nombre: "Pechuga rellena con guarnición",
     Descripcion: "Pechuga de pollo rellena con muzzarela y jamon, con guarnición a elección: Papas Fritas, Pure o ensalada",
     Precio: "2400",
     ID: 5242,
-    Categoria: "Minutas",
     Imagen: "https://www.laalemanacomidas.com.ar/wp-content/uploads/2020/04/180786-a3dd8eb3-b0db-4b1d-bf9f-83c55bc38593-1531908931.png"
 },
 {Nombre: "Bife de lomo con guarnición",
     Descripcion: "Bife de lomo condimentado con hiervas, con guarnición a elección: Papas Fritas, Pure o ensalada",
     Precio: "2500",
     ID: 5243,
-    Categoria: "Minutas",
     Imagen: "https://www.laalemanacomidas.com.ar/wp-content/uploads/2020/04/180786-a3dd8eb3-b0db-4b1d-bf9f-83c55bc38593-1531908931.png"
 },
 {Nombre: "Kippe relleno con guarnición",
     Descripcion: "Kippe relleno con muzzarela, con guarnición a elección: Papas Fritas, Pure o ensalada",
     Precio: "2700",
     ID: 5244,
-    Categoria: "Minutas",
     Imagen: "https://www.laalemanacomidas.com.ar/wp-content/uploads/2020/04/180786-a3dd8eb3-b0db-4b1d-bf9f-83c55bc38593-1531908931.png"
 },
 {Nombre: "Matambre al verdeo con guarnición",
     Descripcion: "Matambre con salsa de verdeo, con guarnición a elección: Papas Fritas, Pure o ensalada",
     Precio: "2500",
     ID: 5245,
-    Categoria: "Minutas",
     Imagen: "https://www.laalemanacomidas.com.ar/wp-content/uploads/2020/04/180786-a3dd8eb3-b0db-4b1d-bf9f-83c55bc38593-1531908931.png"
 },
 {Nombre: "Milanesa napolitana con guarnición",
     Descripcion: "Milanesa con muzzarela, jamon, morron y aceitunas, con guarnición a elección: Papas Fritas, Pure o ensalada",
     Precio: "2600",
     ID: 5246,
-    Categoria: "Minutas",
     Imagen: "https://www.laalemanacomidas.com.ar/wp-content/uploads/2020/04/180786-a3dd8eb3-b0db-4b1d-bf9f-83c55bc38593-1531908931.png"
 }]
 
@@ -138,42 +120,36 @@ let sandwichs = [
     Descripcion: "Sandwich de milanesa con lechuga, tomate y adherezos",
     Precio: 2300,
     ID: 6241,
-    Categoria: "Sandwichs",
     Imagen: "https://www.eltucumano.com/fotos/cache/notas/2023/02/28/818x460_230228193026_74237.jpg"
 },
 {Nombre: "Sandwich de milanesa especial con papas",
     Descripcion: "Sandwich de milanesa con lechuga, tomate, jamón, queso y huevo, acompañado con papas fritas",
     Precio: 2400,
     ID: 6242,
-    Categoria: "Sandwichs",
     Imagen: "https://www.laalemanacomidas.com.ar/wp-content/uploads/2020/04/180786-a3dd8eb3-b0db-4b1d-bf9f-83c55bc38593-1531908931.png"
 },
 {Nombre: "Hamburguesa común",
     Descripcion: "Hamburguesa con lechuga, tomate y adherezos",
     Precio: 2500,
     ID: 6243,
-    Categoria: "Sandwichs",
     Imagen: "https://minitienda.com.ar/administracion/minitiendabackend/products/3aabce96342d9056e084d02d54db7dec.jpg"
 },
 {Nombre: "Hamburguesa especial con papas",
     Descripcion: "Hamburguesa con lechuga, tomate, jamón, queso y huevo, acompañado con papas fritas",
     Precio: 2700,
     ID: 6244,
-    Categoria: "Sandwichs",
     Imagen: "https://www.lavoz.com.ar/resizer/u5rZUvGLNYscfL42u5Xl8T2jpGY=/980x640/smart/filters:quality(75):format(webp)/cloudfront-us-east-1.images.arcpublishing.com/grupoclarin/V2SMBMLC2RE7NGX6LBYMFRADXE.png"
 },
 {Nombre: "Lomito",
     Descripcion: "Lomito con lechuga, tomate y adherezos",
     Precio: 2500,
     ID: 6245,
-    Categoria: "Sandwichs",
     Imagen: "https://cdn.pedix.app/JohCkmWiNdAWwpurKA0H/products/1613746658171.png?size=1500x1500"
 },
 {Nombre: "Lomito especial con papas",
     Descripcion: "Lomito con lechuga, tomate, jamón, queso y huevo, acompañado con papas fritas",
     Precio: 2600,
     ID: 6246,
-    Categoria: "Sandwichs",
     Imagen: "https://www.circuitogastronomico.com/wp-content/uploads/2023/04/pizzar-lomito.jpg"
 }]
 
@@ -184,41 +160,35 @@ let bebidas = [
     Descripcion: "Bebida sin alcohol",
     Precio: "600",
     ID: 7241,
-    Categoria: "Bebidas",
     Imagen: "https://www.laalemanacomidas.com.ar/wp-content/uploads/2020/04/180786-a3dd8eb3-b0db-4b1d-bf9f-83c55bc38593-1531908931.png"
 },
 {Nombre: "Coca-cola 2.5L",
     Descripcion: "Bebida sin alcohol",
     Precio: "700",
     ID: 7242,
-    Categoria: "Bebidas",
     Imagen: "https://www.laalemanacomidas.com.ar/wp-content/uploads/2020/04/180786-a3dd8eb3-b0db-4b1d-bf9f-83c55bc38593-1531908931.png"
 },
 {Nombre: "Lata linea Coca",
     Descripcion: "Bebida sin alcohol",
     Precio: "300",
     ID: 7243,
-    Categoria: "Bebidas",
     Imagen: "https://www.laalemanacomidas.com.ar/wp-content/uploads/2020/04/180786-a3dd8eb3-b0db-4b1d-bf9f-83c55bc38593-1531908931.png"
 },
 {Nombre: "Levite 1.5L",
     Descripcion: "Bebida sin alcohol",
     Precio: "600",
     ID: 7244,
-    Categoria: "Bebidas",
     Imagen: "https://www.laalemanacomidas.com.ar/wp-content/uploads/2020/04/180786-a3dd8eb3-b0db-4b1d-bf9f-83c55bc38593-1531908931.png"
 },
 {Nombre: "Cerveza Quilmes 1L",
     Descripcion: "Bebida alcoholica. Venta solo a mayores de 18 años",
     Precio: "800",
     ID: 7245,
-    Categoria: "Bebidas",
     Imagen: "https://www.laalemanacomidas.com.ar/wp-content/uploads/2020/04/180786-a3dd8eb3-b0db-4b1d-bf9f-83c55bc38593-1531908931.png"
 },
 {Nombre: "Cerveza Stela Artois1L",
     Descripcion: "Bebida alcoholica. Venta solo a mayores de 18 años",
     Precio: "900",
     ID: 7246,
-    Categoria: "Bebidas",
     Imagen: "https://www.laalemanacomidas.com.ar/wp-content/uploads/2020/04/180786-a3dd8eb3-b0db-4b1d-bf9f-83c55bc38593-1531908931.png"
 }]
